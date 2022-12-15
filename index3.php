b3a7dd4d845f1da33ad31e1dd3a91c4d5ed2550d
MMalkowski01 <111887392+MMalkowski01@users.noreply.github.com>
2022-12-15 12:42:16 +0100

    Create index3.php

---
+++ index3.php
@@ -0,0 +1,31 @@
+<html lang="pl" lang="pl">
+<head>
+ <title> AstroFoto </title>
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+ 
+ 
+ <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+ 
+  </head>
+  <?php
+ 	$login=$_GET[login];
+        $haslo=$_GET[haslo]; 
+  ?>
+  
+  
+  <body bgcolor=blue text=white>
+<center>  
+	<?php>
+		$i=0;
+		while ($i<20)
+		{
+			echo "Tu bedzie zdjecie";
+			echo '<br>';
+			$i++;
+		}
+			
+	?>
+</center>
+
+  </body>
+  </html>
