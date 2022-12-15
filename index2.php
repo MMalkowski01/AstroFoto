30f6b08d6dd1dca5a8364e7b5abc0b0d99dc0a02
MMalkowski01 <111887392+MMalkowski01@users.noreply.github.com>
2022-12-15 12:42:00 +0100

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,37 @@
+<html lang="pl" lang="pl">
+<head>
+
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+ 
+ 
+ <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+ 
+  </head>
+  <?php
+ 	$login=$_GET[login];
+        $haslo=$_GET[haslo]; 
+  ?>
+  
+  
+  <body bgcolor="red brick" text=white>
+<center>
+zupa<br>
+zupa<br>
+</center>
+<left>  
+	Profil <br>
+  	<span id ="picture"></span>
+  	<form action="index2.php" method="get">
+  		<input type="text" name="login" value="login"><br>
+  		
+                <input type="text" name="haslo" value=haslo><br>
+                
+  		
+                <input type="submit" value="Zaloguj sie">
+       </form>
+      
+  	          
+
+</left>
+  </body>
+  </html>
