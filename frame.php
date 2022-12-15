69026d4bcd9ba96416ca09400195db3e3bfdc3c9
MMalkowski01 <111887392+MMalkowski01@users.noreply.github.com>
2022-12-15 11:54:59 +0100

    Create frame.php

---
+++ frame.php
@@ -0,0 +1,24 @@
+<html lang="pl" lang="pl">
+<head>
+ <title> AstroFoto </title>
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+ 
+ 
+ <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+ 
+  </head>
+  <?php
+ 	$login=$_GET[login];
+        $haslo=$_GET[haslo]; 
+  ?>
+  
+<FRAMESET cols="150,800">
+	<FRAME noresize="noresize" SRC="index.php">
+	<FRAMESET rows="150,300">
+		<FRAME noresize="noresize" SRC="index2.php">
+		<FRAME noresize="noresize" SRC="index3.php">
+
+</FRAMESET>
+</FRAMESET>
+
+</html>
