9741217672aa2316752ee1362f7b2c9279af91a9
MMalkowski01 <111887392+MMalkowski01@users.noreply.github.com>
2022-12-15 12:42:31 +0100

    Create index4.php

---
+++ index4.php
@@ -0,0 +1,30 @@
+<html lang="pl" lang="pl">
+<head>
+ <title> AstroFoto </title>
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+ 
+ 
+ <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+ 
+  </head>
+  <?php
+ 	$login=$_GET[login];
+        $haslo=$_GET[haslo]; 
+  ?>
+  
+  
+  <body bgcolor=white text=black>
+<center>  
+	<?php>
+		$i=0;
+		while ($i<30)
+		{
+			echo "Tu obszar roboczy";
+			echo '<br>';
+			$i++;
+		}
+			
+	?>
+</center>
+
+  </html>
