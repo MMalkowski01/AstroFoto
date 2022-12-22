3046307d1c2069e93a6a62fe210a07e530bc500a
MMalkowski01 <111887392+MMalkowski01@users.noreply.github.com>
2022-12-22 12:17:32 +0100

    Update frame.php
    Polaczylem 2 framy w jedno, nadalem nazwy framom i zmienilem nazwe src logowania

--- frame.php
+++ frame.php
@@ -7,19 +7,14 @@
  <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
  
   </head>
-  <?php
- 	$login=$_GET[login];
-        $haslo=$_GET[haslo]; 
-  ?>
+ 
   
 <FRAMESET cols="120,800" frameborder="0">
 
 	<FRAMESET rows="75,300">
-	<FRAME scrolling="NO" frameborder="0" noresize="noresize" SRC="index.php">
+	<FRAME scrolling="NO" frameborder="0" noresize="noresize" name="logowanie" SRC="logowanie.php">
 	<FRAME scrolling="NO" frameborder="0" noresize="noresize" SRC="index2.php">
 </FRAMESET>
-	<FRAMESET rows="150,300">
-		<FRAME scrolling="NO" frameborder="0" noresize="noresize" SRC="index3.php">
-		<FRAME scrolling="NO" frameborder="0" noresize="noresize" SRC="index4.php">
-
+		<FRAME scrolling="YES" frameborder="0" noresize="noresize" name="glowna" SRC="index3.php">
+		
 </html>
